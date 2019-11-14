f234b32f0106c5c3c67b8c397c3febcddfafcad3
Malakhova Natasha <8430654@gmail.com>
2019-11-14 05:24:34 +0300

    Create TaskInput.js

---
+++ src/components/TaskInput.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class TaskInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ''
+    };
+  }
+
+  addTask = () => {
+    const { input } = this.state;
+    if(input){
+      this.props.addTask(input);
+      this.setState({ input: ''});
+    }
+  };
+
+  handleEnter = event => {
+    if(event.key === 'Enter') this.addTask();
+  };
+
+  inputChange = event => {
+    this.setState({input: event.target.value});
+  };
+
+  render() {
+    const { input } = this.state;
+    return (
+      <div className="task-input">
+          <input 
+            type="text"
+            onKeyPress={this.handleEnter}
+            onChange={this.inputChange}
+            value={input}
+            placeholder="Например, полить цветы" 
+            autoFocus>
+          </input>
+          <button onClick={this.addTask}>Добавить</button>
+      </div>
+    );
+  }
+}
+
+export default TaskInput;
